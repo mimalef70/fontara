e45447e67c2a8e6901eb571af4745b0b2d96de59
matinsekhavat <matinhyphen@gmail.com>
2024-09-09 11:58:42 +0330

    style: decrease amount of padding for popup wrapper from p-5 to p-4

--- src/popup.tsx
+++ src/popup.tsx
@@ -5,7 +5,7 @@ import SelectListFont from "~components/SelectListFont"
 function IndexPopup() {
   return (
     <div dir="rtl">
-      <div className="w-96 h-96 p-5">
+      <div className="w-96 h-96 p-4">
         <SelectListFont />
       </div>
     </div>

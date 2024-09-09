69ff580851c69c99621b162faf350902c15300b6
matinsekhavat <matinhyphen@gmail.com>
2024-09-09 12:01:53 +0330

    refactor: remove commented Jsx in  SelectListFonts component

--- src/components/SelectListFont.tsx
+++ src/components/SelectListFont.tsx
@@ -1,79 +1,3 @@
-// import {
-//   Listbox,
-//   ListboxButton,
-//   ListboxOption,
-//   ListboxOptions,
-//   Menu,
-//   MenuButton,
-//   MenuItem,
-//   MenuItems
-// } from "@headlessui/react"
-// import {
-//   CheckIcon,
-//   ChevronDownIcon,
-//   PencilIcon
-// } from "@heroicons/react/24/solid"
-// import clsx from "clsx"
-// import { useState } from "react"
-// import ExampleSvg from "react:../../assets/font-samples/estedad.svg"
-
-// import { sendToBackground } from "@plasmohq/messaging"
-
-// const fonts = [
-//   { value: "Estedad", name: "استعداد", svg: ExampleSvg },
-//   { value: "Vazirmatn", name: "وزیر", svg: ExampleSvg }
-// ]
-
-// export default function SelectListFont() {
-//   const [selected, setSelected] = useState(fonts[0])
-
-//   async function handleFontChange(font: (typeof fonts)[0]) {
-//     setSelected(font)
-//     await sendToBackground({
-//       name: "changeFont",
-//       body: { fontName: font.value }
-//     })
-//   }
-//   return (
-//     <div>
-//       <Listbox value={selected} onChange={handleFontChange}>
-//         <ListboxButton
-//           className={clsx(
-//             "relative select select-bordered block w-full rounded-lg bg-black/5 py-1.5 pl-8 p3-3 text-right text-sm/6 text-black",
-//             "focus:outline-none data-[focus]:outline-2 data-[focus]:-outline-offset-2 data-[focus]:outline-black/25"
-//           )}>
-//           {selected.name}
-//         </ListboxButton>
-
-//         <ListboxOptions
-//           anchor="bottom"
-//           transition
-//           className={clsx(
-//             " data-[closed]:scale-95 data-[closed]:opacity-0 w-[var(--button-width)] rounded-xl border border-black/5 bg-black/5 p-1 [--anchor-gap:var(--spacing-1)] focus:outline-none",
-//             "transition duration-100 ease-out data-[leave]:data-[closed]:opacity-0"
-//           )}>
-//           {fonts.map((font) => (
-//             <ListboxOption
-//               key={font.name}
-//               value={font}
-//               className="group flex gap-2 rounded-lg py-1.5 px-3  data-[focus]:bg-black/10">
-//               <CheckIcon className="invisible size-4 fill-black group-data-[selected]:visible" />
-//               <div className="flex items-center space-x-2 rtl:space-x-reverse">
-//                 <PencilIcon className="size-4 fill-gray-400" />
-//                 <span className="font-normal text-sm">{font.name}</span>
-//               </div>
-
-//               <font.svg
-//                 width={90}
-//                 className="text-gray-400 hidden group-hover:inline"
-//               />
-//             </ListboxOption>
-//           ))}
-//         </ListboxOptions>
-//       </Listbox>
-//     </div>
-//   )
-// }
 import {
   Listbox,
   ListboxButton,
@@ -92,9 +16,11 @@ import ExampleSvg from "react:../../assets/font-samples/estedad.svg"
 import { sendToBackground } from "@plasmohq/messaging"
 import { Storage } from "@plasmohq/storage"
 
+import { useFontChange } from "~utils/useFontChange"
+
 const storage = new Storage()
 
-const fonts = [
+export const fonts = [
   { value: "Estedad", name: "استعداد", svg: ExampleSvg },
   { value: "Vazirmatn", name: "وزیر", svg: ExampleSvg },
   { value: "Morraba", name: "مربا", svg: ExampleSvg },
@@ -102,28 +28,25 @@ const fonts = [
 ]
 
 export default function SelectListFont() {
-  const [selected, setSelected] = useState(fonts[0])
+  const { selected, handleFontChange } = useFontChange()
 
+  const [currentTab, setCurrentTab] = useState<string>("")
   useEffect(() => {
-    // Load the selected font from storage when the component mounts
-    storage.get("selectedFont").then((storedFont) => {
-      if (storedFont) {
-        setSelected(fonts.find((font) => font.value === storedFont) || fonts[0])
+    chrome.tabs.query(
+      {
+        active: true,
+        currentWindow: true
+      },
+      (tabs) => {
+        const tab = tabs[0]
+        if (tab.url) {
+          setCurrentTab(tab.url)
+        }
       }
-    })
-  }, [])
-
-  const handleFontChange = async (font: (typeof fonts)[0]) => {
-    setSelected(font)
-    await storage.set("selectedFont", font.value)
-    await sendToBackground({
-      name: "changeFont",
-      body: { fontName: font.value }
-    })
-  }
-
+    )
+  }, [chrome])
   return (
-    <div>
+    <div className="flex flex-col justify-between h-full">
       <Listbox value={selected} onChange={handleFontChange}>
         <ListboxButton
           className={clsx(
@@ -168,6 +91,13 @@ export default function SelectListFont() {
           ))}
         </ListboxOptions>
       </Listbox>
+
+      <div className="border border-gray-400 rounded-md p-2 flex items-center gap-2 select-none">
+        <input type="checkbox" name="activeUrl" id="activeUrl" />
+        <label className="text-base" htmlFor="activeUrl">
+          افزونه فونت آرا برای این سایت فعال شود؟
+        </label>
+      </div>
     </div>
   )
 }

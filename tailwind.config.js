d1deb0166c963197786830faf364adbc37b8be59
matinsekhavat <matinhyphen@gmail.com>
2024-09-23 17:14:04 +0330

    chore: apply all font family for whole project and set into tailwindCSS.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{tsx,html}"],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        estedad: ["estedad", "sans-serif"],
+        mikhak: ["mikhak", "sans-serif"],
+        morabba: ["morabba", "sans-serif"],
+        samim: ["samim", "sans-serif"],
+        shabnam: ["shabnam", "sans-serif"],
+        sahel: ["sahel", "sans-serif"],
+        parastoo: ["parastoo", "sans-serif"],
+        gandom: ["gandom", "sans-serif"],
+        tanha: ["tanha", "sans-serif"],
+        behdad: ["behdad", "sans-serif"],
+        nika: ["nika", "sans-serif"],
+        ganjname: ["ganjname", "sans-serif"],
+        shahab: ["shahab", "sans-serif"]
+      }
+    }
   },
-  plugins: [
-    require('daisyui'),
-  ],  
+  plugins: [require("daisyui")],
   daisyui: {
     themes: ["light"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
     darkTheme: "dark", // name of one of the included themes for dark mode
@@ -15,6 +29,6 @@ module.exports = {
     utils: true, // adds responsive and modifier utility classes
     prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
     logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
-  },
+    themeRoot: ":root" // The element that receives theme color CSS variables
+  }
 }

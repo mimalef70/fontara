d9eaf3526284caa9cdb11823714fa28b42263fa4
Parsa Farahani <parsa.farahani84@gmail.com>
2024-11-24 12:28:50 +0330

    feat: made the add fonts button

--- src/components/BasicVersion.tsx
+++ src/components/BasicVersion.tsx
@@ -399,12 +399,23 @@ export default function BaseVersion() {
       <div>
         {/* Selector Button */}
         <div className="relative">
-          <button
-            className={`relative shadow-md select block w-full rounded-md py-1.5 pl-8 text-right text-sm/6 text-black border border-black/5 transition-all duration-300`}
-            onClick={toggleDropdown}
-          >
-            {selected.name}
-          </button>
+          <div className="flex flex-col gap-3">
+            <button
+              className={`relative shadow-md select block w-full rounded-md py-1.5 pl-8 text-right text-sm/6 text-black border border-black/5 transition-all duration-300`}
+              onClick={toggleDropdown}
+            >
+              {selected.name}
+            </button>
+            <a href="#"
+              className="block flex justify-center items-center gap-1 mb-[15px] font-bold antialiased tracking-[0.2px] bg-[#edf3fd] rounded-[3px] text-[13px] text-[#2374ff] text-center py-[9px] relative"
+            >
+              افزودن فونت دلخواه <svg className="size-5" id="Plus 2" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" clip-rule="evenodd" d="M12 3C16.9709 3 21 7.02908 21 12C21 16.9699 16.9709 21 12 21C7.02908 21 3 16.9699 3 12C3 7.02908 7.02908 3 12 3Z" stroke="#2374ff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M15.2989 12.002H8.69922" stroke="#2374ff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                <path d="M12 8.70386V15.2967" stroke="#2374ff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+              </svg>
+            </a>
+          </div>
 
           {/* Dropdown Menu */}
           {isOpen && (

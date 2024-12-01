eaccec89e6a2406c778a5059887fd1ea7906fb03
Parsa Farahani <parsa.farahani84@gmail.com>
2024-12-01 13:11:09 +0330

    fix: three dots for more than 20 items for url

--- src/components/CustomUrlToggle.tsx
+++ src/components/CustomUrlToggle.tsx
@@ -15,6 +15,11 @@ const CustomUrlToggle = ({
 }: CustomUrlToggleProps) => {
     if (!currentTab) return null
 
+    const displayTabName = () => {
+        const tabName = currentTab.slice(8, -2)
+        return tabName.length > 20 ? `${tabName.slice(0, 20)}...` : tabName
+    }
+
     return (
         <div className="border border-gray-400 rounded-md p-2 select-none mx-auto w-full">
             <label className="text-xs cursor-pointer flex items-center gap-1" htmlFor="activeUrl">
@@ -25,10 +30,11 @@ const CustomUrlToggle = ({
                     onCheckedChange={onToggle}
                 />
                 برای سایت {" "}
-                {currentTab && currentTab.slice(8, -2)} {favicon && <img src={favicon} className="!size-4 object-contain" />}
+                {currentTab && displayTabName()} {favicon && <img src={favicon} className="!size-4 object-contain" />}
                 {" "} فعال باشد؟
             </label>
         </div>
     )
 }
+
 export default CustomUrlToggle

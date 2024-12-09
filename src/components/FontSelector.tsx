e374db8213c26ae5c60b16c332ac9aabb6acefe8
Parsa Farahani <parsa.farahani84@gmail.com>
2024-12-09 15:59:46 +0330

    style: fixed the style of font items

--- src/components/FontSelector.tsx
+++ src/components/FontSelector.tsx
@@ -155,7 +155,6 @@ const FontSelector = ({ setIsActive }) => {
       // console.error("Error handling font change:", error)
     }
   }
-
   return (
     <div className="relative">
       <div className="flex flex-col gap-3">
@@ -165,31 +164,33 @@ const FontSelector = ({ setIsActive }) => {
           onOpenChange={() => setIsActive((prev) => !prev)}
           dir="rtl"
         >
-          <SelectTrigger className="w-full !ring-0 !ring-offset-0 focus:!ring-0 focus:!ring-offset-0 !h-[3rem] !shadow-[0_3px_8px_rgba(0,0,0,0.08)] !transition-all !duration-300 hover:!shadow-[0_10px_20px_rgba(0,0,0,0.15)]">
-            <SelectValue>
-              <span className={`font-estedad text-sm ${selected.style}`}>
-                {selected.name}
-              </span>
-            </SelectValue>
+          <SelectTrigger className="w-full !ring-0 !ring-offset-0 focus:!ring-0 focus:!ring-offset-0 !h-[3rem] !shadow-[0_3px_8px_rgba(0,0,0,0.08)] !transition-all !duration-300 hover:!shadow-[0_10px_20px_rgba(0,0,0,0.15)]">            <SelectValue>
+            <span className={`font-estedad text-sm ${selected.style}`}>
+              {selected.name}
+            </span>
+          </SelectValue>
           </SelectTrigger>
           <SelectContent className="max-h-60 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200 !border-0 !ring-0 !ring-offset-0">
             {allFonts.map((font) => (
-              <SelectItem
+              <div
                 key={font.value}
-                value={font.value}
-                className="flex items-center gap-2 py-1 px-3 cursor-pointer relative"
-                onMouseEnter={() => setHoveredFont(font.value)}
-                onMouseLeave={() => setHoveredFont(null)}>
-                <div className="flex items-center justify-between w-full gap-2">
-                  <span
-                    className={`!w-[7rem] font-estedad text-sm ${font.style} ${selected.value === font.value ? "text-[#0D92F4]" : ""}`}>
-                    {font.name}
-                  </span>
-                  <span
-                    className={`!w-full mx-auto ${font.style} text-gray-400 text-[13px] ${hoveredFont === font.value ? "inline" : "hidden"}`}>
-                    {font.svg}
-                  </span>
-                </div>
+                className="flex items-center justify-between gap-2 relative">
+                <SelectItem
+                  value={font.value}
+                  className="flex items-center gap-2 py-1 px-3 cursor-pointer"
+                  onMouseEnter={() => setHoveredFont(font.value)}
+                  onMouseLeave={() => setHoveredFont(null)}>
+                  <div className="flex items-center justify-between w-full gap-2">
+                    <span
+                      className={`!w-[7rem] font-estedad text-sm ${font.style} ${selected.value === font.value ? "text-[#0D92F4]" : ""}`}>
+                      {font.name}
+                    </span>
+                    <span
+                      className={`!w-full mx-auto ${font.style} text-gray-400 text-[13px] ${hoveredFont === font.value ? "inline" : "hidden"}`}>
+                      {font.svg}
+                    </span>
+                  </div>
+                </SelectItem>
                 <div className="!size-5 fill-black absolute left-2 flex items-center justify-center">
                   {hoveredFont === font.value &&
                     selected.value !== font.value ? (
@@ -198,7 +199,7 @@ const FontSelector = ({ setIsActive }) => {
                     selected.value === font.value && <CheckedCircle />
                   )}
                 </div>
-              </SelectItem>
+              </div>
             ))}
           </SelectContent>
         </Select>

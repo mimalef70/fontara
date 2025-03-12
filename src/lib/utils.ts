b14645b45f9ec47c95742debcace532ce7d25da3
hamedyosefian <hamed77yosefian@gmail.com>
2025-03-12 22:10:08 +0330

    feat: Display version number in Persian format in the header component

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -80,3 +80,8 @@ export async function isUrlActive(currentUrl: string): Promise<boolean> {
 
 //   return false
 // }
+
+export function toPersianNumbers(input: string | number): string {
+  const persianDigits = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"]
+  return input.toString().replace(/\d/g, (x) => persianDigits[parseInt(x)])
+}
